4a78b76220361c87e81b5ec548a98c4e23ddd8eb
Farruxnet <32985353+Farruxnet@users.noreply.github.com>
2023-10-22 15:28:02 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+path = '..'  # files path
+files_list = os.listdir(path)
+
+for file_ in files_list:
+    file_name, extension = os.path.splitext(file_)
+    extension = extension[1:]
+    if extension == '':
+        continue
+    if os.path.exists(path + '/' + extension):
+        os.replace(path + '/' + file_, path + '/' + extension + '/' + file_)
+    else:
+        os.makedirs(path + '/' + extension)
+        os.replace(path + '/' + file_, path + '/' + extension + '/' + file_)
